ee5f929935f9185dc62046cdb320b3c32378182b
minu <gkqkehs10@naver.com>
2023-02-26 22:37:15 +0900

    1726 fail

--- BOJ2/1726.cpp
+++ BOJ2/1726.cpp
@@ -3,50 +3,178 @@
 using namespace std;
 #define MAX 1000000000
 
-int dx[5] = { 0, 1, 0, -1 };
-int dy[5] = { 1, 0, -1, 0};
-int dd[4] = { 4, 1, 3, 2 };
-
 int map[101][101];
-bool visited[101][101];
+bool visited[101][101][5];
 int n, m;
 int x, y, direc;
 int target_x, target_y, target_direc;
 int ans = MAX;
 
 void dfs(int nx, int ny, int nd, int depth) {
+    cout << depth << ": " <<nx << " " << ny << " " << nd << "\n";
     if(nx == target_x && ny == target_y && nd == target_direc) {
-        if(depth < ans) ans = depth;
+        ans = min(ans, depth);
         return;
-    };
-
-    if(nd == 1) {
-
-    } else if (nd == 2) {
-        
-    } else if (nd == 3) {
-
-    } else if (nd == 4) {
-        
     }
-    
-    for(int i=0; i<4; i++) {
-        
-        for(int j=1; j<=3; j++) {
-            int next_x = nx + dx[i] * j;
-            int next_y = ny + dy[i] * j;
-            int next_d = dd[i];
+    int next_x = 0;
+    int next_y = 0;
+    int next_d = 0;
 
-            if(next_x <= n && next_x > 0 && next_y <= n && next_y > 0 && visited[next_x][next_y] == false) {
-                visited[next_x][next_y] = true;
+    // 그 방향 유지
+    next_d = nd;
+    for(int i=0; i<4; i++) {
+        if(next_d == 1) {
+            next_x = nx;
+            next_y = ny + i;
+
+            if(next_x >= 0 && next_x <= n && next_y >=0 && next_y <= m && visited[next_x][next_y][next_d] == false && map[next_x][next_y] == 0) {
+                visited[next_x][next_y][next_d] = true;
+                dfs(next_x, next_y, next_d, depth + 1);
+            }
+        } else if(next_d == 2) {
+            next_x = nx + i;
+            next_y = ny;
+
+            if(next_x >= 0 && next_x <= n && next_y >=0 && next_y <= m && visited[next_x][next_y][next_d] == false && map[next_x][next_y] == 0) {
+                visited[next_x][next_y][next_d] = true;
+                dfs(next_x, next_y, next_d, depth + 1);
+            }
+        } else if(next_d == 3) {
+            next_x = nx;
+            next_y = ny - i;
+
+            if(next_x >= 0 && next_x <= n && next_y >=0 && next_y <= m && visited[next_x][next_y][next_d] == false && map[next_x][next_y] == 0) {
+                visited[next_x][next_y][next_d] = true;
+                dfs(next_x, next_y, next_d, depth + 1);
+            }
+        } else if(next_d == 4) {
+            next_x = nx - i;
+            next_y = ny;
+
+            if(next_x >= 0 && next_x <= n && next_y >=0 && next_y <= m && visited[next_x][next_y][next_d] == false && map[next_x][next_y] == 0) {
+                visited[next_x][next_y][next_d] = true;
                 dfs(next_x, next_y, next_d, depth + 1);
             }
         }
-
-        dfs(nx, ny, dd[i], depth + 1);
     }
 
+    // 오른쪽 회전
+    if(nd == 4) next_d = 1;
+    else next_d = nd +  1;
+    for(int i=0; i<4; i++) {
+        if(next_d == 1) {
+            next_x = nx;
+            next_y = ny + i;
 
+            if(next_x >= 0 && next_x <= n && next_y >=0 && next_y <= m && visited[next_x][next_y][next_d] == false && map[next_x][next_y] == 0) {
+                visited[next_x][next_y][next_d] = true;
+                dfs(next_x, next_y, next_d, depth + 2);
+            }
+        } else if(next_d == 2) {
+            next_x = nx + i;
+            next_y = ny;
+
+            if(next_x >= 0 && next_x <= n && next_y >=0 && next_y <= m && visited[next_x][next_y][next_d] == false && map[next_x][next_y] == 0) {
+                visited[next_x][next_y][next_d] = true;
+                dfs(next_x, next_y, next_d, depth + 2);
+            }
+        } else if(next_d == 3) {
+            next_x = nx;
+            next_y = ny - i;
+
+            if(next_x >= 0 && next_x <= n && next_y >=0 && next_y <= m && visited[next_x][next_y][next_d] == false && map[next_x][next_y] == 0) {
+                visited[next_x][next_y][next_d] = true;
+                dfs(next_x, next_y, next_d, depth + 2);
+            }
+        } else if(next_d == 4) {
+            next_x = nx - i;
+            next_y = ny;
+
+            if(next_x >= 0 && next_x <= n && next_y >=0 && next_y <= m && visited[next_x][next_y][next_d] == false && map[next_x][next_y] == 0) {
+                visited[next_x][next_y][next_d] = true;
+                dfs(next_x, next_y, next_d, depth + 2);
+            }
+        }
+    }
+
+    // 왼쪽 회전
+    if(nd == 0) next_d = 3;
+    else next_d = nd - 1;
+    for(int i=0; i<4; i++) {
+        if(next_d == 1) {
+            next_x = nx;
+            next_y = ny + i;
+
+            if(next_x >= 0 && next_x <= n && next_y >=0 && next_y <= m && visited[next_x][next_y][next_d] == false && map[next_x][next_y] == 0) {
+                visited[next_x][next_y][next_d] = true;
+                dfs(next_x, next_y, next_d, depth + 2);
+            }
+        } else if(next_d == 2) {
+            next_x = nx + i;
+            next_y = ny;
+
+            if(next_x >= 0 && next_x <= n && next_y >=0 && next_y <= m && visited[next_x][next_y][next_d] == false && map[next_x][next_y] == 0) {
+                visited[next_x][next_y][next_d] = true;
+                dfs(next_x, next_y, next_d, depth + 2);
+            }
+        } else if(next_d == 3) {
+            next_x = nx;
+            next_y = ny - i;
+
+            if(next_x >= 0 && next_x <= n && next_y >=0 && next_y <= m && visited[next_x][next_y][next_d] == false && map[next_x][next_y] == 0) {
+                visited[next_x][next_y][next_d] = true;
+                dfs(next_x, next_y, next_d, depth + 2);
+            }
+        } else if(next_d == 4) {
+            next_x = nx - i;
+            next_y = ny;
+
+            if(next_x >= 0 && next_x <= n && next_y >=0 && next_y <= m && visited[next_x][next_y][next_d] == false && map[next_x][next_y] == 0) {
+                visited[next_x][next_y][next_d] = true;
+                dfs(next_x, next_y, next_d, depth + 2);
+            }
+        }
+    }
+
+    // 180도 회전
+    if(nd == 3) next_d = 1;
+    else if(nd == 4) next_d = 2;
+    else next_d = nd + 2;
+    for(int i=0; i<4; i++) {
+        if(next_d == 1) {
+            next_x = nx;
+            next_y = ny + i;
+
+            if(next_x >= 0 && next_x <= n && next_y >=0 && next_y <= m && visited[next_x][next_y][next_d] == false && map[next_x][next_y] == 0) {
+                visited[next_x][next_y][next_d] = true;
+                dfs(next_x, next_y, next_d, depth + 3);
+            }
+        } else if(next_d == 2) {
+            next_x = nx + i;
+            next_y = ny;
+
+            if(next_x >= 0 && next_x <= n && next_y >=0 && next_y <= m && visited[next_x][next_y][next_d] == false && map[next_x][next_y] == 0) {
+                visited[next_x][next_y][next_d] = true;
+                dfs(next_x, next_y, next_d, depth + 3);
+            }
+        } else if(next_d == 3) {
+            next_x = nx;
+            next_y = ny - i;
+
+            if(next_x >= 0 && next_x <= n && next_y >=0 && next_y <= m && visited[next_x][next_y][next_d] == false && map[next_x][next_y] == 0) {
+                visited[next_x][next_y][next_d] = true;
+                dfs(next_x, next_y, next_d, depth + 3);
+            }
+        } else if(next_d == 4) {
+            next_x = nx - i;
+            next_y = ny;
+
+            if(next_x >= 0 && next_x <= n && next_y >=0 && next_y <= m && visited[next_x][next_y][next_d] == false && map[next_x][next_y] == 0) {
+                visited[next_x][next_y][next_d] = true;
+                dfs(next_x, next_y, next_d, depth + 3);
+            }
+        }
+    }
 
 }
 
@@ -59,11 +187,18 @@ int main() {
             int input; 
             cin >> input;
             map[i][j] = input;
-            visited[i][j] = false;
+
+            for(int k=0; k<5; k++) {
+                visited[i][j][k] = false;
+            }
+            
         }
     }
 
     cin >> x >> y >> direc;
-    cin >> target_x, target_y, target_direc;   
+    cin >> target_x >> target_y >> target_direc;  
 
+
+    dfs(x, y, direc, 0);
+    cout << ans;
 }
