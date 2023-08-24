6db81dfdd628acaa5d0bd9760966b37eb342cd72
gkqkehs7 <77993709+gkqkehs7@users.noreply.github.com>
2023-08-24 15:45:35 +0900

    12951

--- Programmers/12951.js
+++ Programmers/12951.js
@@ -1,37 +1,8 @@
 function solution(s) {
-
-    let str_arr = s.split(" ");
-
-    for(let i=0; i<str_arr.length; i++) {
-        let str = str_arr[i];
-
-        if(str === "") continue;
+    return s
+      .toLowerCase()
+      .split(" ")
+      .map((v) => v.charAt(0).toUpperCase() + v.substring(1))
+      .join(" ");
+  }
   
-        let first_str = str[0].toUpperCase();
-
-        let new_str = "";
-
-        if(str.length > 1) {
-            new_str = first_str + str.slice(1).toLowerCase();
-        }
-        
-        str_arr[i] = new_str
-    }
-
-    let answer = "";
-
-    for(let i=0; i<str_arr.length; i++) {
-        if(i !== str_arr.length - 1) {
-            answer += str_arr[i] + " ";
-        } else {
-            answer += str_arr[i];
-        }   
-    }
-  
-
-    // console.log(answer);
-
-    return answer;  
-}
-
-solution(" 3people  unFollowed me")
