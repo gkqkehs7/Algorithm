eb2ca839ae9c11eead3bf22b2982da627edb6951
gkqkehs7 <77993709+gkqkehs7@users.noreply.github.com>
2023-09-02 15:49:05 +0900

    12899

--- Programmers/12899.js
+++ Programmers/12899.js
@@ -1,4 +1,11 @@
 function solution(n) {
-    var answer = '';
+    var answer = "";
+    let mod = ["4","1","2"];
+    
+    while(n > 0){
+        answer = mod[n%3] + answer;
+        n = parseInt((n-1)/3);
+    }
+    
     return answer;
 }
